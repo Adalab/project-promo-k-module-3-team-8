d62c085eb93b7a5bc85f5d09537c5343666dd7fe
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-04 17:54:48 +0100

    Fix indentation

--- src/components/App.js
+++ src/components/App.js
@@ -25,50 +25,52 @@ class App extends React.Component {
               <div className="preview__card">
                 <div className="preview__card--text">
                   <div className="preview__card--stick js-stick stick-border-cold">
-                    <h1 className="js-preview-text js-preview-name preview__card--name name-cold">Nombre y Apellidos</h1>
+                    <h1 className="js-preview-text js-preview-name preview__card--name name-cold">
+                      Nombre y Apellidos
+                    </h1>
                     <h2 className="js-preview-text preview__card--job">Job</h2>
                   </div>
                 </div>
                 <div className="preview__img-container profile__image js__profile-image"></div>
                 <nav>
-                <ul className="preview__contact">
-                  <li>
-                    <a
-                      className="js-preview-href icon-cold preview__contact--link"
-                      href="enlace"
-                      target="_blank"
-                    >
-                      <i className="fas fa-mobile-alt"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="js-preview-href icon-cold preview__contact--link"
-                      href="enlace"
-                      target="_blank"
-                    >
-                      <i className="far fa-envelope"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="js-preview-href icon-cold preview__contact--link"
-                      href="enlace"
-                      target="_blank"
-                    >
-                      <i className="fab fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      className="js-preview-href icon-cold preview__contact--link"
-                      href="enlace"
-                      target="_blank"
-                    >
-                      <i className="fab fa-github-alt"></i>
-                    </a>
-                  </li>
-                </ul>
+                  <ul className="preview__contact">
+                    <li>
+                      <a
+                        className="js-preview-href icon-cold preview__contact--link"
+                        href="enlace"
+                        target="_blank"
+                      >
+                        <i className="fas fa-mobile-alt"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="js-preview-href icon-cold preview__contact--link"
+                        href="enlace"
+                        target="_blank"
+                      >
+                        <i className="far fa-envelope"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="js-preview-href icon-cold preview__contact--link"
+                        href="enlace"
+                        target="_blank"
+                      >
+                        <i className="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        className="js-preview-href icon-cold preview__contact--link"
+                        href="enlace"
+                        target="_blank"
+                      >
+                        <i className="fab fa-github-alt"></i>
+                      </a>
+                    </li>
+                  </ul>
                 </nav>
               </div>
             </div>
