8d7b35b158a198da88a269356626fc0d685956be
Estibaliz Barato <estibalizbarato@gmail.com>
2020-11-11 13:58:43 +0100

    delete formShare doble

--- src/components/main/forms/FormShare.js
+++ src/components/main/forms/FormShare.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React from "react";
 
 class FormShare extends React.Component {
   render() {
     return (
       <>
-        <form className='share js-share'>
-          <i className='far fa-id-card share__icon'></i>
+        <form className="share js-share">
+          <i className="far fa-id-card share__icon"></i>
           <input
-            className='js-submit share__text'
-            type='button'
-            value='Crear Tarjeta'
+            className="js-submit share__text"
+            type="button"
+            value="Crear Tarjeta"
           />
         </form>
 
-        <div className='share-url js-share-url hidden'>
-          <div className='share-url__container'>
-            <h3 className='share-url__title'>La tarjeta ha sido creada:</h3>
+        <div className="share-url js-share-url hidden">
+          <div className="share-url__container">
+            <h3 className="share-url__title">La tarjeta ha sido creada:</h3>
             <a
-              className='js-response share-url__link'
-              target='_blank'
-              rel='noreferrer'
-              href='enlace'
-            ></a>
-            <a
-              className='share-url__link--share'
-              target='_blank'
-              rel='noreferrer'
-              href='https://twitter.com/Adalab_Digital'
+              className="js-response share-url__link"
+              target="_blank"
+              rel="noreferrer"
+              href="enlace"
             >
-              <i className='fab fa-twitter share-url__link--twitter'></i>
-              Compartir en twitter{' '}
+              Aquí aparecerá un enlace
+            </a>
+            <a
+              className="share-url__link--share"
+              target="_blank"
+              rel="noreferrer"
+              href="https://twitter.com/Adalab_Digital"
+            >
+              <i className="fab fa-twitter share-url__link--twitter"></i>
+              Compartir en twitter{" "}
             </a>
           </div>
         </div>
