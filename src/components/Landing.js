2f8f546c6faeb7a2c7261a394ce864bfdcff7f74
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-11-27 16:51:21 +0100

    Add transition and animation to landing

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Footer from './Footer';
-import Image from '../images/logo-NaN-monster.png';
-import './Landing.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Footer from "./Footer";
+import Image from "../images/logo-NaN-monster.png";
+import "./Landing.scss";
+import { Link } from "react-router-dom";
 
 class Landing extends React.Component {
   render() {
     return (
       <>
         <div className="landing">
+          <span className="landing__eye">
+            <p className="landing__eye--point">·</p>
+          </span>
           <main className="landing__main">
             <h1>
               <img
@@ -38,6 +41,9 @@ class Landing extends React.Component {
                 <span>Comparte</span>
               </li>
             </ul>
+            <span className="landing__eye2">
+              <p className="landing__eye2--point">·</p>
+            </span>
             <Link to="/cardgenerator" className="landing__link">
               Comenzar
             </Link>
